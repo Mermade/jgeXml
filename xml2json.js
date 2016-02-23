73c2a9375a89288c7509532b17cbeb334cc84264
unknown <mike.ralphson@gmail.com>
2016-02-23 18:47:18 +0000

    Cater for mixed content and containers per element
    e.g.
    
    <?xml version="1.0" encoding="UTF-8"?>
    <test><blah>4<trouble>4a</trouble>4b</blah></test>

--- xml2json.js
+++ xml2json.js
@@ -13,6 +13,15 @@ String.prototype.insert = function (index, string) {
 		return string + this;
 };
 
+function encode(token) {
+	token = token.replaceAll('\\','\\\\');
+	token = token.replaceAll('\r','\\r');
+	token = token.replaceAll('\n','\\n');
+	token = token.replaceAll('\t','\\t');
+	token = token.replaceAll('"','\\"');
+	return token;
+}
+
 var s;
 
 function getString() {
@@ -21,11 +30,11 @@ function getString() {
 
 function parseString(xml,attributePrefix) {
 
-	var hasContent = false;
+	var hasContent = [];
 	var hasAttribute = false;
 	var lastElement = '';
 	var stack = [];
-	var finished = 0;
+	//var finished = 0;
 	stack.push(1);
 
 	s = '{';
@@ -33,19 +42,31 @@ function parseString(xml,attributePrefix) {
 
 		if (state == jgeXml.sContent) {
 			if (token != '') { // maybe move this in to only omit hasContent = true ??
+				// content should be following a property name not the beginning of an object
+				// so remove assumption it was a container
 				if (s.charAt(s.length-1) == '{') {
 					s = s.substr(0,s.length-1);
 				}
+
+				// if we're following another property, separate with a comma
 				if (s.charAt(s.length-1) == '"') {
 					s += ',';
 				}
+				// if we have had attributes, this is definitely a container not a primitive
+				// treat this value as an anonymous property
 				if (hasAttribute) {
-					s += ' "_" : ';
+					s += ' "' + attributePrefix +'" : ';
 				}
 				else {
-					hasContent = true;
+					if (hasContent[hasContent.length-1]) {
+						//create array
+						if (s.charAt(stack[stack.length-1]) != '[') {
+							s = s.insert(stack[stack.length-1],'[');
+						}
+					}
+					hasContent[hasContent.length-1] = true;
 				}
-				s += '"' + token.replaceAll('"','\\"') + '"';
+				s += '"' + encode(token) + '"';
 			}
 		}
 		else if (state == jgeXml.sEndElement) {
@@ -57,34 +78,39 @@ function parseString(xml,attributePrefix) {
 			if (s.charAt(stack[stack.length-1]) == '[') {
 				s += ']';
 			}
-			//// if we're in an object, close it
-			//if (s.charAt(stack[stack.length-2]) == '{') {
-			//	s += '}';
-			//}
-			// close an empty element
-			if (!hasContent) {
+			//// if we're in an object, close it or if it is empty
+			if ((s.charAt(stack[stack.length-1]) == '{') || (!hasContent[hasContent.length-1])) { //was -2
 				s += '}';
 			}
-			hasContent = false;
-			hasAttribute = false;
+			// this has been folded into the test above, which was uncommented
+			//// close an empty element
+			//if (!hasContent) {
+			//	s += '}';
+			//}
 			lastElement = token;
-			finished = stack.pop();
+			stack.pop();
+			hasContent.pop(); // todo, combine these into an object on one array?
+			
 		}
 		else if (state == jgeXml.sAttribute) {
+			// if not the first attribute, separate the properties with a comma
 			if (s.charAt(s.length-1) !== '{') {
 				s += ',';
 			}
-			s += '"' + attributePrefix + token + '" : ';
+			s += '"' + attributePrefix + token + '": ';
 			hasAttribute = true;
 		}
 		else if (state == jgeXml.sValue) {
-			s += '"' + token.replaceAll('"','\\"') + '"';
+			s += '"' + encode(token) + '"';
 		}
 		else if (state == jgeXml.sElement) {
 			hasAttribute = false;
+			hasContent.push(false);
+			// if this is not the first property, separate with a comma
 			if (s.charAt(s.length-1) !== '{') {
 				s += ',';
 			}
+			// if we're in an array and the property name changes, terminate the array
 			if (token != lastElement) {
 				if (s.charAt(stack[stack.length-1]) == '[') {
 					s = s.insert(s.length-1,']');
@@ -92,7 +118,7 @@ function parseString(xml,attributePrefix) {
 				s += '"' + token + '": ';
 				stack[stack.length-1] = s.length;
 				stack.push(s.length);
-				s += '{';
+				s += '{'; // here we assume all elements are containers not values, this supports attributes
 			}
 			else {
 				// array detected, new element matches previous endElement
