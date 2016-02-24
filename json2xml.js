8b94f9967d3fac7abe2f68bf49ab6f1779ded2de
unknown <mike.ralphson@gmail.com>
2016-02-24 22:16:30 +0000

    WIP convert JSON to XML

---
+++ json2xml.js
@@ -0,0 +1,55 @@
+'use strict';
+
+var xmlWrite = require('./xmlWrite');
+
+var attributePrefix = '@';
+
+function traverse(obj,parent) {
+	
+var result = [];
+	
+	for (var key in obj){
+		// skip loop if the property is from prototype
+		if (!obj.hasOwnProperty(key)) continue;
+
+		var array = Array.isArray(obj);
+		
+		if (typeof obj[key] !== 'object'){
+			if (key.indexOf(attributePrefix)==0) {
+				xmlWrite.attribute(key.substring(1),obj[key]);
+			}
+			else {
+				xmlWrite.startElement(key);
+				xmlWrite.content(obj[key]);
+				xmlWrite.endElement(key);
+			}
+		}
+		else {
+			if (!array) {
+				xmlWrite.startElement(key);
+			}
+			else {
+				//if (key!=0)
+				xmlWrite.startElement(parent);
+			}
+			traverse(obj[key],key);
+			if (!array) {
+				xmlWrite.endElement(key);
+			}
+			else {
+				//if (key!=0) 
+				xmlWrite.endElement(parent);
+			}
+		}
+	}
+	return result;
+}
+
+module.exports = {
+	getXml : function(obj,attrPrefix) {
+		if (attrPrefix) attributePrefix = attrPrefix;
+		xmlWrite.startDocument('UTF8');
+		traverse(obj,'');
+		return xmlWrite.endDocument();
+	}
+}
---
+++ testj2x.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var fs = require('fs');
+var j2x = require('./json2xml');
+
+var filename = process.argv[2];
+
+var json = fs.readFileSync(filename,'utf8');
+console.log(json);
+
+var obj = {};
+try {
+	obj = JSON.parse(json);
+}
+catch (err) {
+	console.error('That is not valid JSON');
+	console.error(err);
+	process.exit(1);
+}
+
+var xml = j2x.getXml(obj);
+console.log();
+console.log(xml);
+console.log();
+console.log(true);
