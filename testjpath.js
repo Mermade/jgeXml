fccabad9e5794cd7109287dfaacdbd597db9414a
Mike Ralphson <mike.ralphson@gmail.com>
2016-02-26 05:11:43 +0000

    Harden against empty input

--- testjpath.js
+++ testjpath.js
@@ -30,23 +30,24 @@ for (var i in tree) {
 	// log our jpath for every item
 	console.log(tree[i].depth+' '+jpath.path(tree[i],false)+' = '+tree[i].value);
 }
-console.log();
 
-var first = jpath.path(tree[1]);
-var matches = jpath.select(tree,first);
-for (var m in matches) {
-	console.log('First; select('+jpath.path(matches[m])+') = '+matches[m].value);
-}
-
-var last = tree[tree.length-1];
-console.log('Last; select('+jpath.path(last,true)+',true) = '+last.value);
-console.log(last.prefix)
-
-var parents = jpath.select(tree,last.prefix);
-if (parents.length>0) {
-	var value = parents[0].value;
-	if (typeof(value) === 'object') {
-		value = JSON.stringify(value,null,2);
+if (tree.length>0) {
+	var first = jpath.path(tree[1]);
+	var matches = jpath.select(tree,first);
+	for (var m in matches) {
+		console.log('First; select('+jpath.path(matches[m])+') = '+matches[m].value);
 	}
-	console.log('select('+jpath.path(parents[0],true)+',true) = '+value);
+
+	var last = tree[tree.length-1];
+	console.log('Last; select('+jpath.path(last,true)+',true) = '+last.value);
+	console.log(last.prefix)
+
+	var parents = jpath.select(tree,last.prefix);
+	if (parents.length>0) {
+		var value = parents[0].value;
+		if (typeof(value) === 'object') {
+			value = JSON.stringify(value,null,2);
+		}
+		console.log('select('+jpath.path(parents[0],true)+',true) = '+value);
+	}
 }
