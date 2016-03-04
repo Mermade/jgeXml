abf22e2ccde744d68560e2c9bbdcb429749e6662
Mike Ralphson <mike.ralphson@gmail.com>
2016-03-04 00:39:22 +0000

    Add test for previous issue and YAML conversion

--- testRunner.js
+++ testRunner.js
@@ -6,6 +6,7 @@ var crypto = require('crypto');
 
 var x2j = require('./xml2json');
 var j2x = require('./json2xml');
+var j2y = require('./json2yaml');
 
 var passing = 0;
 var failing = 0;
@@ -39,7 +40,7 @@ function diff(s1,s2) {
 	var top = l1.length > l2.length ? l2.length : l1.length;
 	for (var l=0;l<top;l++) {
 		if (l1[l] != l2[l]) {
-			
+
 			console.log('Line '+(l+1));
 			var cs = Math.max(0,l-3);
 			for (var c=cs;c<l;c++) {
@@ -61,7 +62,7 @@ function runXmlTest(filename,components) {
 	for (var c=0;c<components.length-1;c++) {
 		stem += (stem ? '.' : '') + components[c];
 	}
-	
+
 	var exists = false;
 	try {
 		fs.statSync('out/'+stem+'.json',fs.R_OK);
@@ -69,14 +70,14 @@ function runXmlTest(filename,components) {
 		exists = true;
 	}
 	catch(err) {}
-		
+
 	if (exists) {
 		var xml = fs.readFileSync('test/'+filename,'utf8');
 		var obj = x2j.xml2json(xml,{"attributePrefix": "@","valueProperty": false, "coerceTypes": false});
 		var json = JSON.stringify(obj,null,2);
 		var compare = fs.readFileSync('out/'+stem+'.json','utf8');
 		compare = compare.replaceAll('\r\n','\n');
-		
+
 		if (json == compare) {
 			passing++;
 		}
@@ -86,7 +87,7 @@ function runXmlTest(filename,components) {
 			failing++;
 		}
 	}
-	
+
 }
 
 function runJsonTest(filename,components) {
@@ -94,7 +95,7 @@ function runJsonTest(filename,components) {
 	for (var c=0;c<components.length-1;c++) {
 		stem += (stem ? '.' : '') + components[c];
 	}
-	
+
 	var	exists = false;
 	try {
 		fs.statSync('out/'+stem+'.xml',fs.R_OK);
@@ -109,7 +110,7 @@ function runJsonTest(filename,components) {
 		var xml = j2x.getXml(obj,'@','',2);
 		var compare = fs.readFileSync('out/'+stem+'.xml','utf8');
 		compare = compare.replaceAll('\r\n','\n');
-		
+
 		if (xml == compare) {
 			passing++;
 		}
@@ -119,8 +120,41 @@ function runJsonTest(filename,components) {
 			failing++;
 		}
 	}
-	
 }
+
+function runYamlTest(filename,components) {
+	var stem = '';
+	for (var c=0;c<components.length-1;c++) {
+		stem += (stem ? '.' : '') + components[c];
+	}
+
+	var	exists = false;
+	try {
+		fs.statSync('out/'+stem+'.yaml',fs.R_OK);
+		console.log('  Convert and compare to YAML');
+		exists = true;
+	}
+	catch(err) {}
+
+	if (exists) {
+		var json = fs.readFileSync('test/'+filename,'utf8');
+		var obj = JSON.parse(json);
+		var yaml = j2y.getYaml(obj);
+		var compare = fs.readFileSync('out/'+stem+'.yaml','utf8');
+		compare = compare.replaceAll('\r\n','\n');
+
+		if (yaml == compare) {
+			passing++;
+		}
+		else {
+			diff(yaml,compare);
+			console.log('  Fail');
+			failing++;
+		}
+	}
+}
+
+process.exitCode = 1; // in case of crash
 
 var tests = fs.readdirSync('test');
 for (var t in tests) {
@@ -132,6 +166,7 @@ for (var t in tests) {
 	}
 	else if (components[components.length-1] == 'json') {
 		runJsonTest(filename,components);
+		runYamlTest(filename,components);
 	}
 }
 
